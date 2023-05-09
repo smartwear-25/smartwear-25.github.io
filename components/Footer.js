5daa04bd105eed4db45b6112f6ca6ae5ab9b8967
dongma0a <madongxx@gmail.com>
2023-05-09 16:00:06 +0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -2,9 +2,9 @@ import cselogo from "../public/CSELOGO.svg";
 import schoollogo from "../public/unswlogo.svg";
 import acmlogo from "../public/acmlogo.svg";
 import Image from "next/image";
-export default function Footer() {
+ {/*export default function Footer() {
   return (
-    {/*<>
+     <>
       <div className="bg-blue-200 container h-8 mt-2">
         <p className="justify-center text-center text-xl font-semibold">
           Sponsor
@@ -32,6 +32,6 @@ export default function Footer() {
           </a>
         </div>
       </div>
-    </>*/}
+    </> 
   );
-}
+}*/}
