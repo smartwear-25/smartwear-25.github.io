0842e6802fe9ad770bf13557692f6cad29e0b06d
LaserHu <39296171+LaserHu@users.noreply.github.com>
2023-05-09 15:09:54 +0800

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -25,9 +25,11 @@ function Navigation() {
               <Nav className="me-auto">
                 <Link href="/" className="no-underline px-2 py-2 text-gray-400 hover:text-gray-200">
                     Home
-                </Link>
-                <Link href="/call_for_papers" className="no-underline px-2 py-2 text-gray-400 hover:text-gray-200">
+                <Link href="/call_for_papers">
+                  <a className="no-underline px-2 py-2 text-gray-400 hover:text-gray-200">
                     Call for papers
+                  </a>
+                </Link>
                 </Link>
                 <Link href="/committees" className="no-underline px-2 py-2 text-gray-400 hover:text-gray-200">
                   Committees
