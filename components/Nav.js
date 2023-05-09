9c0be440d1f7b2437c1ae7a7309b8039ac2b0638
LaserHu <39296171+LaserHu@users.noreply.github.com>
2023-05-09 15:17:34 +0800

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -17,34 +17,42 @@ function Navigation() {
             <Head>
               <title>SmartWear 2022 | ACM MobiCom 2022</title>
             </Head>
-            <Link href="/" className="no-underline text-xl text-white mr-4">
-              SmartWear
+            <Link href="/">
+              <a className="no-underline text-xl text-white mr-4">SmartWear</a>
             </Link>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="me-auto">
-                <Link href="/" className="no-underline px-2 py-2 text-gray-400 hover:text-gray-200">
+                <Link href="/">
+                  <a className="no-underline px-2 py-2 text-gray-400 hover:text-gray-200">
                     Home
+                  </a>
                 </Link>
                 <Link href="/call_for_papers">
                   <a className="no-underline px-2 py-2 text-gray-400 hover:text-gray-200">
                     Call for papers
                   </a>
                 </Link>
-                <Link href="/committees" className="no-underline px-2 py-2 text-gray-400 hover:text-gray-200">
-                  Committees
+                <Link href="/committees">
+                  <a className="no-underline px-2 py-2 text-gray-400 hover:text-gray-200">
+                    Committees
+                  </a>
                 </Link>
-                <Link href="/technical_program" className="no-underline px-2 py-2 text-gray-400 hover:text-gray-200">
-                  Program
+                <Link href="/technical_program">
+                  <a className="no-underline px-2 py-2 text-gray-400 hover:text-gray-200">
+                    Program
+                  </a>
                 </Link>
-                <Link href="/paper_submission" className="no-underline px-2 py-2 text-gray-400 hover:text-gray-200">
-                  Paper Submission
+                <Link href="/paper_submission">
+                  <a className="no-underline px-2 py-2 text-gray-400 hover:text-gray-200">
+                    Paper Submission
+                  </a>
                 </Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
-        <div className="banner relative z-0" style={{ height: "25vw" }}>
+        <div className="banner relative z-0" style={{ height: "20vw" }}>
           <Image
             className="object-cover"
             alt="banner"
@@ -53,23 +61,23 @@ function Navigation() {
           />
           <div
             className="leading-tight object-contain overflow-hidden backdrop-blur-sm relative top-1/3 text-white z-1 mx-auto text-center align-middle"
-            style={{ height: "15vw", width: "50vw" }}
+            style={{ height: "12vw", width: "50vw" }}
           >
             <p className="mb-0" style={{ fontSize: "2vw" }}>
-              SmartWear 2023
+              SmartWear 2022
             </p>
             <p className="mb-0" style={{ fontSize: "1.5vw" }}>
               Workshop on Smart Wearable Systems and Applications
             </p>
             <p className="mb-0" style={{ fontSize: "1.2vw" }}>
-              In Conjunction with MobiCom 2023
+              In Conjunction with MobiCom 2022
             </p>
             <p className="mb-0 " style={{ fontSize: "1.5vw" }}>
-              2-6 Oct 2023, Madrid, Spain
+              October, 17, 2022, Sydney
             </p>
             <p className="mb-0 font-serif" style={{ fontSize: "1.4vw" }}>
               ( SmartWear will run as a hybrid event, which provides an option
-              for presenters and participants to attend the workshop in person )
+              for presenters and participants to attend the workshop virtually )
             </p>
           </div>
         </div>
