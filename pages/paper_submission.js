552acd91855cbb2b4dabd9628885f41044462eb1
LaserHu <39296171+LaserHu@users.noreply.github.com>
2023-06-01 20:05:57 +0800

    Update paper_submission.js

--- pages/paper_submission.js
+++ pages/paper_submission.js
@@ -47,9 +47,8 @@ export default function paperSumisstion() {
         <div className="text-justify font-sans break-word pb-2">
           <h2 className="pt-2">Important Dates</h2>
           <p className="pt-2 justify-center text-left text-lg text-red-500 ">TBD</p>
-          {/*<ul className="list-disc pl-5">
-            <li>Abstract Registration: <span className="font-bold">June 12, 2022</span> </li> 
-            {/*<li>
+          {<ul className="list-disc pl-5">
+            {<li>
               <s>
                 Submission Deadline:{" "}
                 <span className="font-bold">June 12, 2023</span>
@@ -76,7 +75,7 @@ export default function paperSumisstion() {
             <li>
               Workshop: <span className="font-bold">October 6, 2023</span>
             </li>
-          </ul>*/}
+          </ul>}
         </div>
       </div>
     </>
