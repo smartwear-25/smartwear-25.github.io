b31c683fd2945894d1e2e5df90eaef843a1315ec
LaserHu <39296171+LaserHu@users.noreply.github.com>
2023-06-02 08:58:38 +0800

    Update paper_submission.js

--- pages/paper_submission.js
+++ pages/paper_submission.js
@@ -8,30 +8,32 @@ export default function paperSumisstion() {
       <div className="container mx-auto">
         <div className="text-justify font-sans break-word pb-2 pt-2 inline">
           <h2 className="pt-2">Submission Instructions</h2>
-        <div className="text-justify">
-          Demo proposals must be accompanied with an additional 1-page
-          description of the precise setup and requirements (the 1-page setup
-          description will not be published in the proceedings). Reviews will be
-          single-blinded. Papers should neither have been published elsewhere
-          nor being currently under review by another conference or journal.
-          Papers should contain original material, namely, material that has not
-          been previously published or currently submitted for consideration
-          elsewhere. Prospective authors are encouraged to submit a single PDF
-          file with all fonts embedded, using the ACM conference proceedings
-          format. (Use the same PDF{" "}
-          <a
-            className="text-red-500 no-underline"
-            href="https://sigmobile.org/mobicom/2023/cfp.html"
-          >
-            formatting guidelines
-          </a>{" "}
-          as the main conference.) Paper length is limited to six (6) pages
-          (two-column, 10-point format) including references, figures, etc.
-          Papers must include the author names and affiliations for single-blind
-          peer reviewing by the program committee. Authors of accepted papers
-          are expected to present their papers at the workshop.
+          <div>
+            Demo proposals must be accompanied with an additional 1-page
+            description of the precise setup and requirements (the 1-page setup
+            description will not be published in the proceedings). Reviews will
+            be single-blinded. Papers should neither have been published
+            elsewhere nor being currently under review by another conference or
+            journal. Papers should contain original material, namely, material
+            that has not been previously published or currently submitted for
+            consideration elsewhere. Prospective authors are encouraged to
+            submit a single PDF file with all fonts embedded, using the ACM
+            conference proceedings format. (Use the same PDF{" "}
+            <a
+              className="text-red-500 no-underline"
+              href="https://sigmobile.org/mobicom/2023/cfp.html"
+            >
+              formatting guidelines
+            </a>{" "}
+            as the main conference.) Paper length is limited to six (6) pages
+            (two-column, 10-point format) including references, figures, etc.
+            Papers must include the author names and affiliations for
+            single-blind peer reviewing by the program committee. Authors of
+            accepted papers are expected to present their papers at the
+            workshop.
+          </div>
         </div>
-        <div className="text-justify font-sans break-word pb-2">
+        <div className="text-justify font-sans break-word pb-2 pt-1">
           <h2>Submission Website</h2>
           <p className="pt-2 justify-center text-left text-lg text-red-500 ">TBD</p>
           {/*<a
