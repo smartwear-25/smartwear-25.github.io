173afd366467f2266f3fd872b174f0f659b499d8
LaserHu <39296171+LaserHu@users.noreply.github.com>
2023-08-02 15:24:51 +0800

    Update call_for_papers.js

--- pages/call_for_papers.js
+++ pages/call_for_papers.js
@@ -136,11 +136,11 @@ export default function callForPapers() {
             </li>
             <li>
               Acceptance Notification:{" "}
-              <span className="font-bold">August 4, 2023</span>
+              <span className="font-bold">August 2, 2023</span>
             </li>
             <li>
               Camera Ready Due:{" "}
-              <span className="font-bold">August 11, 2023</span>
+              <span className="font-bold">August 9, 2023</span>
             </li>
             <li>
               Workshop Date: <span className="font-bold">October 6, 2023</span>
